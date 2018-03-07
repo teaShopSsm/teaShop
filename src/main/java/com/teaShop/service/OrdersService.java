500d2c4fc2227105f432d28bf7b76708fd3d3d83
44510214@qq.com <0701sxx>
2018-03-07 22:18:31 +0800

    订单

---
+++ src/main/java/com/teaShop/service/OrdersService.java
@@ -0,0 +1,14 @@
+package com.teaShop.service;
+
+import com.teaShop.bean.TeaOrders;
+
+import java.util.List;
+
+public interface OrdersService {
+
+    /**
+     * 查询所有订单
+     * @return
+     */
+    public List<TeaOrders> getAll();
+}
