b84292ad47015a1608bb4530bd0ae0e515ac0cba
44510214@qq.com <0701sxx>
2018-03-09 21:27:59 +0800

    首页插件

---
+++ src/main/java/com/teaShop/mapper/TeaOrderGoodsMapper.java
@@ -0,0 +1,15 @@
+/*
+* TeaOrderGoodsMapper.java
+* http://www.xxx.com
+* Copyright © 2018 xxx All Rights Reserved
+* 作者：
+* QQ：
+* E-Mail：
+* 2018-03-08 22:00 Created
+*/ 
+package com.teaShop.mapper;
+import com.teaShop.bean.TeaOrderGoods;
+import com.teaShop.orm.Mapper;
+
+public interface TeaOrderGoodsMapper extends Mapper<TeaOrderGoods> {
+}
